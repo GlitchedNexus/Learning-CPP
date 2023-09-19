6794304e802c05918005ed10ef88f9f258aaf1e5
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2023-09-19 16:43:00 -0700

    Added Tutorial: While Loops

---
+++ Introduction/Loops/whileLoops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+
+    /*
+    
+    The while Loop:
+
+    This loop keeps iterating for as long as the passed condition is active.
+    The syntax for a while loop is as follows:
+
+    while (<condition>) {
+        // code block
+    }
+    
+    NOTE: You must have some internal updates for the loop condition or you
+    will end up with an infinite loop.
+    */
+
+    while (name.empty()) {
+        std::cout << "Enter your name: ";
+        std::cin  >> name;
+    }
+
+    return 0;
+
+}
