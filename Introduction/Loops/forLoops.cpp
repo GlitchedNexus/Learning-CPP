bbff4d18038d27ab71d7217caa8e4c10109e53d1
Glitched Nexus <143273662+GlitchedNexus@users.noreply.github.com>
2023-09-20 14:19:36 -0700

    Added Tutorial: For Loops

---
+++ Introduction/Loops/forLoops.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+
+    /*
+    
+    The for Loop:
+
+    This loop keeps iterating for a pre-specified number of interations
+    The syntax for a for loop is as follows:
+
+    for(<loop variable>; <loop condition>; <update condition>) {
+        // Code Block
+    }
+
+    */
+
+   for (int i = 0; i <= 3; i++) {
+        std::cout << "Current Iteration Number " << i << "\n";
+   }
+
+    return 0;
+
+}
